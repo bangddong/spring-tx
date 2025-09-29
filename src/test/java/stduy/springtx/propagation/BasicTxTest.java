9e84f8e3257d97613d111a3531017beba48e1268
bangddong <gusehdqkd@naver.com>
2025-09-29 21:14:28 +0900

    전파 예제

--- src/test/java/stduy/springtx/propagation/BasicTxTest.java
+++ src/test/java/stduy/springtx/propagation/BasicTxTest.java
@@ -76,7 +76,7 @@ public class BasicTxTest {
 	}
 
 	@Test
-	void inner_rollback() {
+	void inner_commit() {
 		log.info("외부 트랜잭션 시작");
 		TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
 		log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
@@ -90,4 +90,19 @@ public class BasicTxTest {
 		log.info("외부 트랜잭션 커밋");
 		txManager.commit(outer);
 	}
+
+	@Test
+	void inner_rollback() {
+		log.info("외부 트랜잭션 시작");
+		TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+		log.info("내부 트랜잭션 시작");
+		TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+		log.info("내부 트랜잭션 커밋");
+		txManager.commit(inner);
+
+		log.info("외부 트랜잭션 롤백");
+		txManager.rollback(outer);
+	}
+
 }
