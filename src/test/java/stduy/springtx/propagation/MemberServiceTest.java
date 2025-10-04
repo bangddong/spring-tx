bc8d7319c34caae7c888f8addcbc6263f4695bec
bangddong <gusehdqkd@naver.com>
2025-10-04 18:11:34 +0900

    개발자 실수 포인트. 명시적인 Exception 처리를 통해 로그와 회원 가입을 분리하고자 하지만 정상 동작하지 않음.

--- src/test/java/stduy/springtx/propagation/MemberServiceTest.java
+++ src/test/java/stduy/springtx/propagation/MemberServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -91,5 +92,43 @@ class MemberServiceTest {
 		// then
 		assertTrue(memberRepository.find(username).isPresent());
 		assertTrue(logRepository.find(username).isPresent());
+	}
+
+	/**
+	 * MemberService @Transactional: ON
+	 * MemberRepository @Transactional: ON
+	 * LogRepository @Transactional: ON Exception
+	 */
+	@Test
+	public void outerTxOn_fail() throws Exception {
+		// given
+		String username = "로그예외_outerTxOn_fail";
+
+		// when
+		assertThatThrownBy(() -> memberService.joinV1(username))
+			.isInstanceOf(RuntimeException.class);
+
+		// then: 모든 데이터 롤백
+		assertTrue(memberRepository.find(username).isEmpty());
+		assertTrue(logRepository.find(username).isEmpty());
+	}
+
+	/**
+	 * MemberService @Transactional: ON
+	 * MemberRepository @Transactional: ON
+	 * LogRepository @Transactional: ON Exception
+	 */
+	@Test
+	public void recoverException_fail() throws Exception {
+		// given
+		String username = "로그예외_recoverException_fail";
+
+		// when
+		assertThatThrownBy(() -> memberService.joinV2(username))
+			.isInstanceOf(UnexpectedRollbackException.class);
+
+		// then: 모든 데이터 롤백
+		assertTrue(memberRepository.find(username).isEmpty());
+		assertTrue(logRepository.find(username).isEmpty());
 	}
 }
